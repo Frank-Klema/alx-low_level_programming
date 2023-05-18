b4e1a8881546f9ad51dd8b88e9088b429ed8fb0c
Frank-Klema <frankchukwunonso@gmail.com>
2023-05-18 10:49:37 -0700

    put strings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - prints string
+ * @void: no input
+ *
+ * Return: returns 0
+ */
 
-int main()
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 
