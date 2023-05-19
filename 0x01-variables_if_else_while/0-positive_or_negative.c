7f4a721519575a6c2cf915f2212aed6210143be2
Frank-Klema <frankchukwunonso@gmail.com>
2023-05-19 10:06:31 -0700

    c control structures project

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - randomly prints a number indicating it's sign
+ * @void: no input
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		printf("%d is negative", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else
+		printf("%d is positive", n);
+	print("\n");
+
+	return (0);
+}
