41604a8993dcff46f4e13ac802a763c93a46ec4c
Frank-Klema <frankchukwunonso@gmail.com>
2023-05-25 20:09:04 -0700

    meet the syntax requirement

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_square - draw a diagonal line on the terminal
+ * print_square - draw a square
  * @size: an integer
+ *
  */
 
 void print_square(int size)
 {
 	int w, b;
 
-	if (size > 0)
+	if (size >= 1)
 	{
 		for (w = 1; w <= size; w++)
 		{
@@ -20,6 +21,7 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
+
 	else
 	{
 		_putchar('\n');
