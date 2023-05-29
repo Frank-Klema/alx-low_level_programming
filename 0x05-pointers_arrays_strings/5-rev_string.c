b3d3230045c595f6afa0786ee1f05e3f2864d658
Frank-Klema <frankchukwunonso@gmail.com>
2023-05-29 15:44:33 -0700

    testing string reverse function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: is a pointer to char
+ */
+
+void rev_string(char* s)
+{
+	char a;
+	int i = 0, j = 0, half;
+
+	while (*(s + i))
+	{
+		i++;
+	}
+	i--;
+	half = i / 2;
+
+	while ((*(s + i)) && i >= half)
+	{
+		a = *(s + i);
+		*(s + i) = *(s + j);
+		*(s + j) = a;
+		i--;
+		j++;
+	}
+			
+}
