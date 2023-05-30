771feb59671d473fc6b51d7f84665183a49141ca
Frank-Klema <frankchukwunonso@gmail.com>
2023-05-30 02:38:50 -0700

    correct error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,9 +10,9 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		return (c);
+		return (1);
 	}
 	else
 		return (0);
